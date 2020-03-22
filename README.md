d5006fdba7cb1beba7c5a09a910f16d3bf2e534f
Heron Yang <heron.yang.tw@gmail.com>
2020-03-22 16:37:22 -0700

    Add JS linter.

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ $ docker run -p 49160:8080 -d heronyang/vimnote
 ```
 
 It runs on [localhost:49160](http://localhost:49160)
+
+## Lint
+
+```bash
+$ npx standard --fix
+```
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
 // App
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index', {message: "Hello World!"});
-});
+  res.render('index', { message: 'Hello World!' })
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
