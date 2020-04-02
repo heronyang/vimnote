9cdfab31e3880c38418175add337486347f83a03
Heron Yang <heron.yang.tw@gmail.com>
2020-04-01 22:04:57 -0700

    Refactor callbacks into a standalone function.

--- server/server.js
+++ server/server.js
@@ -2,24 +2,23 @@
 require('dotenv').config()
 
 const db = require('./db')
-const app = require("express")()
 const crypto = require("crypto")
 
-// Returns "ok".
-app.get("/health", (req, res) => {
+const checkHealth = (req, res) => {
   res.send("ok");
-})
+}
 
-// Creates a new doc entry and returns its doc_id.
-app.get("/create", (req, res) => {
+const createDoc = (req, res) => {
   // TODO: Make sure there's no duplication by catching errors.
   const doc_id = crypto.randomBytes(5).toString('hex');
   db.insertDoc(doc_id, /*title=*/"", /*content=*/"");
   res.send(doc_id);
-});
+}
+
+const app = require("express")()
+app.get("/health", checkHealth)
+app.get("/create", createDoc)
 
 // Starts server.
-const port = process.env.SERVER_PORT
-const host = process.env.SERVER_HOST
-app.listen(port, host);
+app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST);
 console.log(`Running on http://${host}:${port}`)
