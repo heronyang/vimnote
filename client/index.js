17d22afa6525b46d341096bb8ef9ab156c75aad2
Heron Yang <heron.yang.tw@gmail.com>
2020-04-21 21:10:31 -0700

    Implemented a very hacky e2e prototype

--- client/index.js
+++ client/index.js
@@ -4,7 +4,67 @@ const CodeMirror = require("codemirror");
 require("codemirror/mode/markdown/markdown");
 require("codemirror/keymap/vim");
 
+// Initializes UI components.
 const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   mode: "markdown",
   keyMap: "vim",
 });
+
+// Initializes the content based on URL.
+function handleRootRequest() {
+  // Create a new doc.
+  // TODO: read url from env variable.
+  // TODO: move calls to another js file. this file focus on user interactions.
+  $.post("http://0.0.0.0:8888/create", function(data) {
+    // Redirects to the doc page created.
+    console.log(data);
+    window.location = url.origin + "/" + data;
+  }).fail(function() {
+    console.error("create new doc.");
+  });
+}
+
+function handleDocRequest(docId) {
+  console.log(docId);
+  $.get("http://0.0.0.0:8888/download?docId=" + docId, function(data) {
+    // Loads the data content onto UI.
+    console.log(data);
+    // TODO: title is not working yet as it's always empty
+    document.title = data.title;
+    editor.getDoc().setValue(data.content);
+    saveDoc(); // just for debugging
+  }).fail(function() {
+    console.error("upload doc failed.");
+  });
+}
+
+var jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+
+const url = new URL(document.URL);
+if (url.pathname == '/') {
+  handleRootRequest();
+} else {
+  // Try to load an old doc.
+  handleDocRequest(url.pathname.slice(1));
+}
+
+// Handles the user's input.
+$('#save').click(saveDoc);
+function saveDoc() {
+  // Flushes the local content to server.
+  const docId = getDocId();
+  const content = editor.getDoc().getValue();
+  $.post("http://0.0.0.0:8888/upload",
+    {docId: docId, content: content, title: 'fake title'}, function(data) {
+    console.log('saved');
+  }).fail(function() {
+    console.error("save doc failed.");
+  });
+}
+
+function getDocId() {
+  // TODO: handle many kinds of corner cases.
+  const url = new URL(document.URL);
+  return url.pathname.slice(1);
+}
--- server/server.js
+++ server/server.js
@@ -9,12 +9,14 @@ const checkHealth = (req, res) => {
 
 const createDoc = (req, res) => {
   // TODO: Make sure there's no duplication by catching errors.
+  console.log('Create Doc')
   const docId = crypto.randomBytes(5).toString('hex')
   db.writeDoc(docId, /* title= */'', /* content= */'',
     function () { res.send(docId) })
 }
 
-const updateDoc = (req, res) => {
+const uploadDoc = (req, res) => {
+  console.log('Upload Doc')
   const docId = req.body.docId
   if (!docId) {
     res.send('Missing docId.')
@@ -25,7 +27,8 @@ const updateDoc = (req, res) => {
   db.writeDoc(docId, title, content, function () { res.send('ok') })
 }
 
-const loadDoc = (req, res) => {
+const downloadDoc = (req, res) => {
+  console.log('Download Doc')
   const docId = req.query.docId
   if (!docId) {
     res.send('Missing docId.')
@@ -40,10 +43,16 @@ const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.get('/health', checkHealth)
 app.post('/create', createDoc)
-app.post('/update', updateDoc)
-app.get('/load', loadDoc)
+app.post('/upload', uploadDoc)
+app.get('/download', downloadDoc)
 
 // Starts server.
 const port = process.env.SERVER_PORT
